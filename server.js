2c2b2bc5aa6bb0a70186e568a31119c794702b47
tjkapur <77752459+tjkapur@users.noreply.github.com>
2022-02-17 15:46:21 -0500

    added basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+var port = 5001
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+})
+
+app.use(function(req, res) {
+    res.status(404).send("Endpoint does not exist")
+    res.type("text/plain")
+})
