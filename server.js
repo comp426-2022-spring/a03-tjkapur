13e42bdd730361ef06833a4a82a673039e6344af
tjkapur <77752459+tjkapur@users.noreply.github.com>
2022-02-17 16:34:29 -0500

    added endpoint

--- server.js
+++ server.js
@@ -7,11 +7,22 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port))
 })
 
+function coinFlip() {
+    return Math.random() > .5 ? 'heads' : 'tails';
+}
+
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 })
 
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({'message': req.params.number})
+})
+
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({'flip': coinFlip()})
+})
 
 app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist")
